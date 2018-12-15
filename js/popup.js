038499f9ac0fc0f1afe3335fc2b6ece4f2e84e3e
Natallia Viazenkina <bluewolf@tut.by>
2018-12-16 01:14:00 +0300

    Анимация формы

---
+++ js/popup.js
@@ -0,0 +1,52 @@
+var feedback = document.querySelector(".contacts-feedback");
+var popup = document.querySelector(".modal-feedback");
+var overlay = document.querySelector(".overlay");
+var close = popup.querySelector(".modal-close");
+var login = popup.querySelector("[name=feedback-login]");
+var form = popup.querySelector("form");
+var email = popup.querySelector("[name=feedback-email]");
+var message = popup.querySelector("textarea");
+
+
+
+feedback.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-closing");
+	popup.classList.add("modal-show");
+	overlay.classList.add("overlay-show");
+	login.focus();
+	
+
+  });
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+	popup.classList.add("modal-closing");
+	overlay.classList.remove("overlay-show");
+
+});
+
+ form.addEventListener("submit", function (evt) {
+    if (!email.value || !message.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } 
+
+  });
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		if (popup.classList.contains("modal-show")) {
+			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error");
+			popup.classList.add("modal-closing");
+			overlay.classList.remove("overlay-show");
+		}
+	}
+});
+ 
