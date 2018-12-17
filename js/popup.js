9175b803bf7615a79e3772dd3e812503b4eafabe
Natallia Viazenkina <bluewolf@tut.by>
2018-12-18 00:12:20 +0300

    Добавлены вендорные префиксы
    + исправлена ошибка отображения высоты полей ввода в ie

--- js/popup.js
+++ js/popup.js
@@ -7,17 +7,12 @@ var form = popup.querySelector("form");
 var email = popup.querySelector("[name=feedback-email]");
 var message = popup.querySelector("textarea");
 
-
-
 feedback.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-closing");
 	popup.classList.add("modal-show");
 	overlay.classList.add("overlay-show");
-	login.focus();
-	
-
-  });
+});
 
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -25,18 +20,16 @@ close.addEventListener("click", function (evt) {
 	popup.classList.remove("modal-error");
 	popup.classList.add("modal-closing");
 	overlay.classList.remove("overlay-show");
-
 });
 
- form.addEventListener("submit", function (evt) {
-    if (!email.value || !message.value) {
-      evt.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-    } 
-
-  });
+form.addEventListener("submit", function (evt) {
+	if (!email.value || !message.value) {
+		evt.preventDefault();
+		popup.classList.remove("modal-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");
+	} 
+});
 
 window.addEventListener("keydown", function (evt) {
 	if (evt.keyCode === 27) {
@@ -49,4 +42,3 @@ window.addEventListener("keydown", function (evt) {
 		}
 	}
 });
- 
